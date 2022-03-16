7d31c656349b90c2a010a368fb9202a963ae47a2
jaunn-victorr <joao.vitor.ptal@gmail.com>
2022-03-16 17:56:23 +0000

    aula16-03

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+ public static void main (String[] args) {
+     int numeros[];
+     numeros = new int [10];
+
+     int novos[] = new int [10];
+
+     String frutas[] = {"laranja", "abacaxi", "uva"};
+     //vetores em java sempre são tamanhos fixos
+
+    
+    //  system.out.println(frutas[0]);
+    //  system.out.println(frutas[1]);
+    //  system.out.println(frutas[2]);
+    int cont = 0; 
+    while(cont < 3) {
+       
+        System.out.println(frutas[cont]);
+        cont++;
+    }
+for(int i = 0;1 < 3;i++) {
+    System.out.println(frutas[i]);
+}
+
+ }
+}
